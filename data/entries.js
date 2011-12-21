039ad6733ef60432905312f0eb71e3009c3210e8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-12-21 19:44:20 +0100

    [FIX] typo
    the word "timestamp" has a "s" in it.

--- data/entries.js
+++ data/entries.js
@@ -1,13 +1,13 @@
 [
 	{
 		"type": "entry",
-		"timetamp": "2011-12-21T13:37:00Z",
+		"timestamp": "2011-12-21T13:37:00Z",
 		"title": "First Entry",
 		"content": "This is the first entry in the journal."
 	},
 	{
 		"type": "entry",
-		"timetamp": "2011-12-21T13:37:01Z",
+		"timestamp": "2011-12-21T13:37:01Z",
 		"title": "Second Entry",
 		"content": "This is the second entry in the journal."
 	}
